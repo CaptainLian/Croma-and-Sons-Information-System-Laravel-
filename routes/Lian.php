53217d1521b41673f51bfd988df2dc4d4dc06dab
Lian <lian_laguio@yahoo.com.ph>
2016-10-21 16:25:41 +0800

    Fixed Routes
    Added includes to routes and 2 separate files

---
+++ routes/Lian.php
@@ -0,0 +1,28 @@
+<?php
+
+Route::get('/', ['as' => 'LoginScreen', 'uses' => 'PageController@index']);
+
+
+/* FORMS */
+Route::post('/', ['as' => 'LoginValidation', 'uses' => 'LoginController@validateLogin']);
+
+Route::get('/sales/dashboard', ['as' => 'SalesDashboard', 
+							  'uses' => 'BusinessControllers\SalesPageController@viewDashboard']);
+
+Route::get('/procurement/dashboard', ['as' => 'ProcurementDashboard',
+									 'uses' => 'BusinessControllers\ProcurementPageController@viewDashboard']);
+
+Route::get('/inventory/dashboard', ['as' => 'InventoryDashboard', 
+							      'uses' => 'BusinessControllers\InventoryPageController@viewDashboard']);
+
+Route::get('/admin/dashboard', ['as' => 'InventoryDashboard', 
+							      'uses' => 'BusinessControllers\AdminPageController@viewDashboard']);
+
+
+Route::get('/logout', ['as' => 'logout', 'uses' => 'LoginController@logout']);
+
+
+/* Procurement */
+
+Route::get('/procurement/PurchaseOrder', ['as' => 'PurchaseOrder', 'uses' => 'BusinessControllers\ProcurementPageController@viewPurchaseOrder']);
+?>
